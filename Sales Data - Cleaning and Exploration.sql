cbdcbf4a1a9a29dffbcf37d33a61cfe76ab29a6e
V-ci <96588497+V-ci@users.noreply.github.com>
2024-03-16 12:22:28 +0000

    Update Sales Data - Cleaning and Exploration.sql

--- Sales Data - Cleaning and Exploration.sql
+++ Sales Data - Cleaning and Exploration.sql
@@ -1,7 +1,7 @@
 /* 
 Data Cleaning and Data Exploration Project Portfolio
 
-Skills used: ALTER, CREATE, RENAME, UNION, JOINS, CTE, Window Functions, Aggregate Functions, COnverting Data Types
+Skills used: ALTER, CREATE, RENAME, UNION, JOINS, CTE, Window Functions, Aggregate Functions, Converting Data Types
 
 */
 
